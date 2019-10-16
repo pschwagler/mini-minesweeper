6d9dbc8894dab43e3b3e81e6c1353c31198911f0
pschwagler <pschwagl@gmail.com>
2019-10-16 10:55:42 -0400

    added bomb png

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -6,42 +6,56 @@ const generateClass = cellData => {
   if (cellData.status === 'DEFAULT') {
     return 'unclicked';
   } else if (cellData.status === 'UNCOVERED') {
-    if (cellData.isBomb) {
-      return 'bombed';
-    } else {
-      if (cellData.number === 0) {
-        return 'cleared';
-      } else if (cellData.number === 1) {
-        return 'cleared one';
-      } else if (cellData.number === 2) {
-        return 'cleared two';
-      } else if (cellData.number === 3) {
-        return 'cleared three';
-      } else if (cellData.number === 4) {
-        return 'cleared four';
-      } else if (cellData.number === 5) {
-        return 'cleared five';
-      } else if (cellData.number === 6) {
-        return 'cleared six';
-      } else if (cellData.number === 7) {
-        return 'cleared seven';
-      } else if (cellData.number === 8) {
-        return 'cleared eight';
-      }
+    if (cellData.number === 0) {
+      return 'cleared';
+    } else if (cellData.number === 1) {
+      return 'cleared one';
+    } else if (cellData.number === 2) {
+      return 'cleared two';
+    } else if (cellData.number === 3) {
+      return 'cleared three';
+    } else if (cellData.number === 4) {
+      return 'cleared four';
+    } else if (cellData.number === 5) {
+      return 'cleared five';
+    } else if (cellData.number === 6) {
+      return 'cleared six';
+    } else if (cellData.number === 7) {
+      return 'cleared seven';
+    } else if (cellData.number === 8) {
+      return 'cleared eight';
     }
+  } else if (cellData.status === 'BOMBED') {
+    return 'cleared';
   }
 };
 
-const Cell = ({ cellData, coords, handleClick }) => (
-  <div
-    className={generateClass(cellData)}
-    onClick={() => {
-      handleClick(coords);
-    }}
-  >
-    {cellData.isBomb ? 'B' : cellData.number === 0 ? '' : cellData.number}
-  </div>
-);
+const Cell = ({ cellData, coords, handleClick }) => {
+  if (cellData.status === 'DEFAULT') {
+    return (
+      <div
+        className={generateClass(cellData)}
+        onClick={() => {
+          handleClick(coords);
+        }}
+      >
+        {' '}
+      </div>
+    );
+  } else if (cellData.status === 'UNCOVERED') {
+    return (
+      <div className={generateClass(cellData)}>
+        {cellData.number === 0 ? '' : cellData.number}
+      </div>
+    );
+  } else if (cellData.status === 'BOMBED') {
+    return (
+      <div className={generateClass(cellData)}>
+        <img src='bomb-logo.png' className='cell-img'></img>
+      </div>
+    );
+  }
+};
 
 export default connect(
   (state, { coords }) => ({
